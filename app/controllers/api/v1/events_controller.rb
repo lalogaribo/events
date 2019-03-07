ed69619c3e2930a507a63dffd04850754778f261
eduardo <lalo>
2019-03-06 20:43:35 -0600

    Fixed event creation action and added callback to find event

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,51 +1,52 @@
 class Api::V1::EventsController < ApplicationController
-  def index
-    if params.has_key?(:category_id)
-      events = Event.where(category_id: params[:category_id])
-    else
-      events =  Event.all
-    end
-    render json: events
-  end
+	before_action :find_event, only: [:update, :show, :destroy]
 
-  def new; end
+	def index
+		if params.has_key?(:category_id)
+			events = Event.where(category_id: params[:category_id])
+		else
+			events = Event.all
+		end
+		render json: events
+	end
 
-  def create
-    event = Event.new(event_params)
-    event.user = current_user
-    if event.save
-      render json: event
-    else
-      render json: event.errors.full_messages, status: :unprocessable_entity
-    end
-  end
+	def create
+		event = Event.new(event_params)
+		if event.save
+			render json: event
+		else
+			render json: event.errors.full_messages, status: :unprocessable_entity
+		end
+	end
 
-  def update
-    event = Event.find(params[:id])
-    event.update(event_params)
-    if event.save
-      render json: event, status: :ok
-    else
-      render json: event.errors.full_messages, status: :unprocessable_entity
-    end
-  end
+	def update
+		@event.update(event_params)
+		if @event.save
+			render json: @event, status: :ok
+		else
+			render json: @event.errors.full_messages, status: :unprocessable_entity
+		end
+	end
 
-  def show
-    render json: Event.find(params[:id]), status: 200
-  end
+	def show
+		render json: @event, status: 200
+	end
 
-  def destroy
-    event = Event.find(params[:id])
-    if event.destroy
-      render json: event, status: :ok
-    else
-      render json event.errors.full_messages, status: :not_found
-    end
-  end
+	def destroy
+		if @event.destroy
+			render json: @event, status: :ok
+		else
+			render json @event.errors.full_messages, status: :not_found
+		end
+	end
 
-  private
+	private
 
-  def event_params
-    params.require(:event).permit(:name, :description, :address, :category_id)
-  end
+		def event_params
+			params.require(:event).permit(:name, :description, :address, :category_id, :user_id)
+		end
+
+		def find_event
+			@event = Event.find(params[:id])
+		end
 end 
