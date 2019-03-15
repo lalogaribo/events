b6d27e6c17e83023d3bbbb36aebc9171df598b8c
eduardo <lalo>
2019-03-14 23:44:20 -0600

    Created create_user endpoint

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,18 @@
+module Api::V1
+  class UsersController < ApplicationController
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: @user.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :phone_number)
+    end
+  end
+end
